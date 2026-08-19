87a35bd34ca51d8803896e2864a0af3100d46258
Hassan Aguezoum <haguezoum@gmail.com>
2026-08-19 01:08:52 +0100

    Add the Analytics component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Background from "./components/backGround";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
@@ -99,6 +100,7 @@ function App() {
         </div>
 
       </div>
+      <Analytics />
     </>
   );
 }
